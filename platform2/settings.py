8774df139f974d07de7bbc153381129665764e1f
Your Name <you@example.com>
2021-03-08 19:31:45 +0600

    static root fix

--- platform2/settings.py
+++ platform2/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/app/static'
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
