5b784169f8d766e338b5f7ac921621fe028c57ae
Your Name <you@example.com>
2021-03-08 19:29:51 +0600

    static root fix

--- platform2/settings.py
+++ platform2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/app/static'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
