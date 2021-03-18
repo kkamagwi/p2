d0cb7c4d8d2a4b42325901fc7e9f901631799132
Your Name <you@example.com>
2021-03-18 13:48:41 +0600

    hw redused

--- platform2/settings.py
+++ platform2/settings.py
@@ -136,8 +136,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(
-    BASE_DIR, "static"),)
+# STATICFILES_DIRS = (os.path.join(
+#     BASE_DIR, "static"),)
 STATIC_ROOT = os.path.join(
     os.path.dirname(BASE_DIR), "deployment", "collected_static")
 MEDIA_ROOT = os.path.join(
